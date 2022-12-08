435f73f7ae06bd0b6826bbefbaefb49b9b59296e
Nicolas Gasull <nicolas@gasull.net>
2022-12-08 16:12:53 +0000

    Publish latest blog

---
+++ docs/_next/static/ove7atG6cNxhZsWdZj6EC/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fblog\u002F[slug]","\u002Ffr","\u002Ffr\u002Fabout","\u002Ffr\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
